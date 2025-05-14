7d73b932514a7f93cfe574b625390f6d742317a2
Lucas Henrique Gonçalves Souto <lucashenriquegoncalvessouto1@gmail.com>
2025-05-14 12:30:35 -0300

    feat:Criação do controller da ia

---
+++ src/main/java/com/backend/cyberbytes/controller/IaController.java
@@ -0,0 +1,30 @@
+package com.backend.cyberbytes.controller;
+
+import com.backend.cyberbytes.service.IaService;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+@RestController
+@RequestMapping("/ia")
+public class IaController {
+
+    private IaService service;
+
+    @PostMapping("/chat")
+    public ResponseEntity<String> enviar_prompt(@RequestParam("prompt") String prompt) throws URISyntaxException, IOException, InterruptedException {
+        try{
+            String resposta = service.fazerRequisicao(prompt);
+            return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(resposta);
+        } catch (Exception e) {
+            System.out.println("Erro ao tentar fazer requiisção. Exeption: "+ e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+}
